ab47644c69f9925a8e859bb94cf311d56a0696a2
Ezequiel Anokian <EAnokia1@its.jnj.com>
2025-07-01 14:29:51 +0200

    updated dev branch
    - added renv.lock and renv/
    - added .Renviron and .Rprofile
    - minor bugfix in title_footers.R
    - devtools::document() updated a few man/*.Rd
    - devtools::test() updated a few snapshots
    - manually updated DESCRIPTION from 0.0.1 to 0.1.0

--- R/title_footers.R
+++ R/title_footers.R
@@ -13,11 +13,12 @@
 #' @importFrom utils read.csv
 .read_titles_file <- function(file) {
   if (grepl("csv$", file, ignore.case = TRUE)) {
-    df <- read.csv(file)
+    df <- read.csv(file, check.names = FALSE)
   } else if (grepl("xlsx$", file, ignore.case = TRUE)) {
-    if (!requireNamespace("readxl"))
+    if (!requireNamespace("readxl")) {
       stop("readxl package is required for xslx file support, please install it.")
-    df <- readxl::read_excel(df, sheet = 1, range = readxl::cell_cols("A:C"))
+    }
+    df <- readxl::read_excel(path = file, sheet = 1, range = readxl::cell_cols("A:C"))
   } else {
     stop("Unrecognized titles file type. file: ", file)
   }
@@ -86,6 +87,8 @@ get_titles_from_file <- function(id,
 
     if (length(title) != 1) {
       msg <- "Warning: Title file should contain exactly one title record per Table ID"
+    } else {
+      message(file)
     }
 
     main_footer <- title_df[grep("^FOOT", title_df$IDENTIFIER), ]$TEXT
