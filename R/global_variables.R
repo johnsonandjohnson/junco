5c7b12cb93f1a462a986d2ee3ef2d07a628b6bc7
munoztd0 <david.munoztord@mailbox.org>
2025-07-04 12:05:38 +0200

    cleaning for CI/CD

--- R/global_variables.R
+++
@@ -1,2 +0,0 @@
-## needed for functions output by rtables::make_split_fun
-utils::globalVariables("core_split")
--- R/junco-package.R
+++
@@ -1,7 +0,0 @@
-#' @import formatters
-#' @import rtables
-#' @import tern
-#' @import tidytlg
-#' @import tibble
-#' @import methods
-NULL
