31592477d58a5a328aecd9827ade49efeb2a2489
Ezequiel Anokian <lainjoso@hotmail.com>
2025-07-21 10:24:21 +0200

    added test-colwidths.R, added unit tests for pool_rbmi()

---
+++ tests/testthat/test-colwidths.R
@@ -0,0 +1,74 @@
+
+fontspec <- font_spec("Times", 9L, 1)
+ADSL <- data.frame(
+    USUBJID = c(
+        "XXXXX01", "XXXXX02", "XXXXX03", "XXXXX04", "XXXXX05",
+        "XXXXX06", "XXXXX07", "XXXXX08", "XXXXX09", "XXXXX10"
+    ),
+    TRT01P = factor(
+        c(
+            "ARMA", "ARMB", "ARMA", "ARMB", "ARMB",
+            "Placebo", "Placebo", "Placebo", "ARMA", "ARMB"
+        )
+    ),
+    FASFL = c("Y", "Y", "Y", "Y", "N", "Y", "Y", "Y", "Y", "Y"),
+    SAFFL = c("N", "N", "N", "N", "N", "N", "N", "N", "N", "N"),
+    PKFL = c("N", "N", "N", "N", "N", "N", "N", "N", "N", "N")
+)
+
+lyt <- basic_table() |>
+    split_cols_by("TRT01P") |>
+    add_overall_col("Total") |>
+    analyze("FASFL",
+            var_labels = "Analysis set:",
+            afun = a_freq_j,
+            extra_args = list(label = "Full", val = "Y"),
+            show_labels = "visible"
+    ) |>
+    analyze("SAFFL",
+            var_labels = "Analysis set:",
+            afun = a_freq_j,
+            extra_args = list(label = "Safety", val = "Y"),
+            show_labels = "visible"
+    ) |>
+    analyze("PKFL",
+            var_labels = "Analysis set:",
+            afun = a_freq_j,
+            extra_args = list(label = "PK", val = "Y"),
+            show_labels = "visible"
+    )
+
+tt <- build_table(lyt, ADSL)
+
+test_that("ttype_wrap_vec works as expected", {
+    
+    
+    result <- ttype_wrap_vec(vec = c(1, 2, 3, 4) %>% as.character(), fontspec = fontspec, width = 2)
+    expected_result <- list(
+        c("1"),
+        c("2"),
+        c("3"),
+        c("4")
+    )
+    testthat::expect_equal(result, expected_result)
+    
+})
+
+test_that("check_wrap_nobreak works as expected", {
+    
+    result <- check_wrap_nobreak(tt = tt, colwidths = rep(20, 5), fontspec = fontspec)
+    testthat::expect_true(result)
+    
+    result <- check_wrap_nobreak(tt = tt, colwidths = rep(1, 5), fontspec = fontspec)
+    testthat::expect_false(result)
+    
+})
+
+test_that("smart_colwidths_1page works as expected", {
+    
+    result <- smart_colwidths_1page(tt = tt, fontspec = fontspec)
+    expected_result <- c(58, 29, 29, 29, 29)
+    testthat::expect_equal(result, expected_result)
+})
+
+
