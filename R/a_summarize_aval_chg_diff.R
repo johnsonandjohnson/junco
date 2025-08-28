60b376df0b8c2b3ab8274d2fb4b3f7dd0bce00df
RMao6 <rmao6@its.jnj.com>
2025-08-28 11:38:57 +0000

    Use small data frame

--- R/a_summarize_aval_chg_diff.R
+++ R/a_summarize_aval_chg_diff.R
@@ -288,71 +288,93 @@ format_xxd <- function(str, d = 0, .df_row, formatting_fun = NULL) {
 #'
 #'
 #' @examples
-#' if (require("pharmaverseadamjnj")) {
-#'   library(dplyr)
+#' library(dplyr)
 #'
-#'   ADEG <- pharmaverseadamjnj::adeg |>
-#'     select(STUDYID, USUBJID, TRT01A, PARAM, AVISIT, AVAL, CHG) |>
-#'     filter(PARAM == "ECG Mean Heart Rate (beats/min)") |>
-#'     mutate(colspan_trt = factor(
-#'       if_else(TRT01A == "Placebo", " ", "Active Study Agent"),
-#'       levels = c("Active Study Agent", " ")
-#'     )) |>
-#'     mutate(rrisk_header = "Risk Difference (%) (95% CI)") |>
-#'     mutate(rrisk_label = paste(TRT01A, paste("vs", "Placebo")))
+#' ADEG <- data.frame(
+#'   STUDYID = c(
+#'     "DUMMY", "DUMMY", "DUMMY", "DUMMY", "DUMMY",
+#'     "DUMMY", "DUMMY", "DUMMY", "DUMMY", "DUMMY"
+#'   ),
+#'   USUBJID = c(
+#'     "XXXXX01", "XXXXX02", "XXXXX03", "XXXXX04", "XXXXX05",
+#'     "XXXXX06", "XXXXX07", "XXXXX08", "XXXXX09", "XXXXX10"
+#'   ),
+#'   TRT01A = c(
+#'     "ARMA", "ARMA", "ARMA", "ARMA", "ARMA", "Placebo",
+#'     "Placebo", "Placebo", "ARMA", "ARMA"
+#'   ),
+#'   PARAM = c("BP", "BP", "BP", "BP", "BP", "BP", "BP", "BP", "BP", "BP"),
+#'   AVISIT = c(
+#'     "Visit 1", "Visit 1", "Visit 1", "Visit 1", "Visit 1",
+#'     "Visit 1", "Visit 1", "Visit 1", "Visit 1", "Visit 1"
+#'   ),
+#'   AVAL = c(56, 78, 67, 87, 88, 93, 39, 87, 65, 55),
+#'   CHG = c(2, 3, -1, 9, -2, 0, 6, -2, 5, 2)
+#' )
 #'
-#'   colspan_trt_map <- create_colspan_map(ADEG,
-#'     non_active_grp = "Placebo",
-#'     non_active_grp_span_lbl = " ",
-#'     active_grp_span_lbl = "Active Study Agent",
-#'     colspan_var = "colspan_trt",
-#'     trt_var = "TRT01A"
+#' ADEG <- ADEG |>
+#'   mutate(
+#'     TRT01A = as.factor(TRT01A),
+#'     STUDYID = as.factor(STUDYID)
 #'   )
-#'   ref_path <- c("colspan_trt", " ", "TRT01A", "Placebo")
 #'
-#'   lyt <- basic_table() |>
-#'     split_cols_by(
-#'       "colspan_trt",
-#'       split_fun = trim_levels_to_map(map = colspan_trt_map)
-#'     ) |>
-#'     split_cols_by("TRT01A") |>
-#'     split_rows_by(
-#'       "PARAM",
-#'       label_pos = "topleft",
-#'       split_label = "Blood Pressure",
-#'       section_div = " ",
-#'       split_fun = drop_split_levels
-#'     ) |>
-#'     split_rows_by(
-#'       "AVISIT",
-#'       label_pos = "topleft",
-#'       split_label = "Study Visit",
-#'       split_fun = drop_split_levels,
-#'       child_labels = "hidden"
-#'     ) |>
-#'     split_cols_by_multivar(
-#'       c("AVAL", "AVAL", "CHG"),
-#'       varlabels = c("n/N (%)", "Mean (CI)", "CFB (CI)")
-#'     ) |>
-#'     split_cols_by("rrisk_header", nested = FALSE) |>
-#'     split_cols_by(
-#'       "TRT01A",
-#'       split_fun = remove_split_levels("Placebo"),
-#'       labels_var = "rrisk_label"
-#'     ) |>
-#'     split_cols_by_multivar(c("CHG"), varlabels = c(" ")) |>
-#'     analyze("STUDYID",
-#'       afun = a_summarize_aval_chg_diff_j,
-#'       extra_args = list(
-#'         format_na_str = "-", d = 0,
-#'         ref_path = ref_path, variables = list(arm = "TRT01A", covariates = NULL)
-#'       )
+#' ADEG$colspan_trt <- factor(ifelse(ADEG$TRT01A == "Placebo", " ", "Active Study Agent"),
+#'   levels = c("Active Study Agent", " ")
+#' )
+#' ADEG$rrisk_header <- "Risk Difference (%) (95% CI)"
+#' ADEG$rrisk_label <- paste(ADEG$TRT01A, paste("vs", "Placebo"))
+#'
+#' colspan_trt_map <- create_colspan_map(ADEG,
+#'   non_active_grp = "Placebo",
+#'   non_active_grp_span_lbl = " ",
+#'   active_grp_span_lbl = "Active Study Agent",
+#'   colspan_var = "colspan_trt",
+#'   trt_var = "TRT01A"
+#' )
+#' ref_path <- c("colspan_trt", " ", "TRT01A", "Placebo")
+#'
+#' lyt <- basic_table() |>
+#'   split_cols_by(
+#'     "colspan_trt",
+#'     split_fun = trim_levels_to_map(map = colspan_trt_map)
+#'   ) |>
+#'   split_cols_by("TRT01A") |>
+#'   split_rows_by(
+#'     "PARAM",
+#'     label_pos = "topleft",
+#'     split_label = "Blood Pressure",
+#'     section_div = " ",
+#'     split_fun = drop_split_levels
+#'   ) |>
+#'   split_rows_by(
+#'     "AVISIT",
+#'     label_pos = "topleft",
+#'     split_label = "Study Visit",
+#'     split_fun = drop_split_levels,
+#'     child_labels = "hidden"
+#'   ) |>
+#'   split_cols_by_multivar(
+#'     c("AVAL", "AVAL", "CHG"),
+#'     varlabels = c("n/N (%)", "Mean (CI)", "CFB (CI)")
+#'   ) |>
+#'   split_cols_by("rrisk_header", nested = FALSE) |>
+#'   split_cols_by(
+#'     "TRT01A",
+#'     split_fun = remove_split_levels("Placebo"),
+#'     labels_var = "rrisk_label"
+#'   ) |>
+#'   split_cols_by_multivar(c("CHG"), varlabels = c(" ")) |>
+#'   analyze("STUDYID",
+#'     afun = a_summarize_aval_chg_diff_j,
+#'     extra_args = list(
+#'       format_na_str = "-", d = 0,
+#'       ref_path = ref_path, variables = list(arm = "TRT01A", covariates = NULL)
 #'     )
+#'   )
 #'
-#'   result <- build_table(lyt, ADEG)
+#' result <- build_table(lyt, ADEG)
 #'
-#'   result
-#' }
+#' result
 #' @seealso s_summarize_ancova_j
 #' @family Inclusion of ANCOVA Functions
 a_summarize_aval_chg_diff_j <- function(
