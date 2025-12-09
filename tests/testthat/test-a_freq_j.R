2658c9e358c3bb48c338ed33fd5decfe2fc03781
Ilse <55379552+iaugusty@users.noreply.github.com>
2025-12-09 14:37:12 +0100

    address bug #120: a_freq_j() - risk diff. Suspected results in the presence of upper row-split and no alt_counts_df (#123)
    * resolve bug and add test
    
    * [skip style] [skip vbump] Restyle files
    
    * Trigger CI: empty commit
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/testthat/test-a_freq_j.R
+++ tests/testthat/test-a_freq_j.R
@@ -78,3 +78,47 @@ test_that("a_freq_j_with_exclude allows to exclude row split levels from the ana
     safe_prune_table(prune_func = tern::keep_rows(keep_non_null_rows))
   expect_snapshot(result)
 })
+
+test_that("a_freq_j in specific situation error for not passing alt_counts_df", {
+  library(dplyr)
+  trtvar <- "ARM"
+  ctrl_grp <- "B: Placebo"
+
+  adsl <- ex_adsl |> select(c("USUBJID", "STRATA1", "EOSSTT", all_of(trtvar)))
+  adsl$colspan_trt <- factor(
+    ifelse(adsl[[trtvar]] == ctrl_grp, " ", "Active Study Agent"),
+    levels = c("Active Study Agent", " ")
+  )
+  adsl$rrisk_header <- "Risk Difference (%) (95% CI)"
+  adsl$rrisk_label <- paste(adsl[[trtvar]], paste("vs", ctrl_grp))
+
+  colspan_trt_map <- create_colspan_map(
+    df = adsl,
+    non_active_grp = ctrl_grp,
+    non_active_grp_span_lbl = " ",
+    active_grp_span_lbl = "Active Study Agent",
+    colspan_var = "colspan_trt",
+    trt_var = trtvar
+  )
+
+  a_freq_j_args <- list(
+    .stats = "count_unique_fraction",
+    ref_path = c("colspan_trt", " ", trtvar, ctrl_grp)
+  )
+
+  lyt <- basic_table(show_colcounts = TRUE) |>
+    split_cols_by("colspan_trt", split_fun = trim_levels_to_map(map = colspan_trt_map)) |>
+    split_cols_by(trtvar) |>
+    split_cols_by("rrisk_header", nested = FALSE) |>
+    split_cols_by(trtvar, labels_var = "rrisk_label", split_fun = remove_split_levels(ctrl_grp)) |>
+    split_rows_by("STRATA1") |>
+    analyze("EOSSTT", afun = a_freq_j, extra_args = a_freq_j_args)
+
+  expect_error(
+    build_table(lyt, adsl),
+    "In order to get correct numbers in relative risk column"
+  )
+
+  result <- build_table(lyt, adsl, alt_counts_df = adsl)
+  expect_snapshot(result)
+})
