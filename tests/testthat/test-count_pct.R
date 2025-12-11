d7d7dfb3a5d82fa2eff13d735ced7e0cb0fac4ef
Ezequiel Anokian <lainjoso@hotmail.com>
2025-12-11 10:30:09 +0100

    minor tweaks in unit tests test-count_pct.R and test-varia.R to make it pass the CICD pipeline

--- tests/testthat/test-count_pct.R
+++ tests/testthat/test-count_pct.R
@@ -402,8 +402,7 @@ test_that("a_freq_j with N_trt as denom - special situation", {
     ) %>%
     select(USUBJID, TRTEMFL, ASEV, AEBODSYS, AEBODSYSx, AEDECOD)
 
-  adaeall <- adaeall %>%
-    inner_join(., adsl_ %>% select(USUBJID, ARM), by = c("USUBJID"))
+  adaeall <- inner_join(adaeall, adsl_ %>% select(USUBJID, ARM), by = c("USUBJID"))
 
   adaeall$spanheader <- factor(
     ifelse(adaeall$ASEV == "Total", " ", "Severity"),
