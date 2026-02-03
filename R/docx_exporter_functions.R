10903ebd1bfce54a16b97ca3409a0c0d8b42b250
Ezequiel Anokian <lainjoso@hotmail.com>
2026-02-03 16:15:57 +0100

    Update docx_exporter_functions.R

--- R/docx_exporter_functions.R
+++ R/docx_exporter_functions.R
@@ -712,6 +712,7 @@ tt_to_flextable_j <- function(
       pg_width = pg_width_by_orient(orientation == "landscape"),
       pg_height = NULL,
       margins = rep(0, 4),
+      lpp = NULL,
       nosplitin = nosplitin,
       verbose = FALSE,
       round_type = round_type
