11d6643d409b03d84253baa0f977ec46358e8232
Ezequiel Anokian <lainjoso@hotmail.com>
2026-02-17 10:40:40 +0100

    for consistency, fake watermark is applied to tables, listings and graphs

--- R/docx_exporter_functions.R
+++ R/docx_exporter_functions.R
@@ -90,30 +90,35 @@ insert_fake_watermark_XML <- function(doc, watermark, orientation) {
 
   checkmate::assert_character(watermark, len = 1)
   checkmate::assert_choice(orientation, choices = c("portrait", "landscape"))
+  
 
   # find out where to insert the node
-  node <- xml2::xml_find_first(doc$doc_obj$get(), ".//w:tbl")
-  node <- xml2::xml_find_first(node, ".//w:tr")
-  node <- xml2::xml_find_first(node, ".//w:tc")
-  node <- xml2::xml_find_first(node, ".//w:p")
-  node <- xml2::xml_find_first(node, ".//w:r")
-
-  if (orientation == "landscape") {
-    v1 <- -464704
-    v2 <- 1947315
-    v3 <- -36.6
-    v4 <- 153.35
-  } else {
-    v1 <- -1414153
-    v2 <- 3758927
-    v3 <- -111.35
-    v4 <- 296
-  }
-
-  # insert the node
-  # nolint start
-  node_to_insert <- paste0('
-    						<w:r>
+  nodes <- xml2::xml_find_all(doc$doc_obj$get(), ".//w:tbl")
+  for (node in nodes) {
+    node <- xml2::xml_find_first(node, ".//w:tr")
+    node <- xml2::xml_find_first(node, ".//w:tc")
+    node <- xml2::xml_find_first(node, ".//w:p")
+    node <- xml2::xml_find_first(node, ".//w:r")
+    
+    if (orientation == "landscape") {
+      v1 <- -464704 # positionH
+      v2 <- 1947315 # positionV
+      v3 <- -36.6 # margin-left
+      v4 <- 153.35 # margin-top
+    } else {
+      v1 <- -1414153
+      v2 <- 3758927
+      v3 <- -111.35
+      v4 <- 296
+    }
+    rot_angle <- (360-45)*60000
+    w_alpha <- 77000
+    
+    
+    # insert the node
+    # nolint start
+    node_to_insert <- paste0('
+            <w:r>
 							<w:rPr>
 								<w:noProof/>
 								<w:color w:val="000000"/>
@@ -179,7 +184,7 @@ insert_fake_watermark_XML <- function(doc, watermark, orientation) {
 																				<w14:textFill>
 																					<w14:solidFill>
 																						<w14:srgbClr w14:val="000000">
-																							<w14:alpha w14:val="77000"/>
+																							<w14:alpha w14:val="', w_alpha, '"/>
 																						</w14:srgbClr>
 																					</w14:solidFill>
 																				</w14:textFill>
@@ -195,12 +200,12 @@ insert_fake_watermark_XML <- function(doc, watermark, orientation) {
 																				<w14:textFill>
 																					<w14:solidFill>
 																						<w14:srgbClr w14:val="000000">
-																							<w14:alpha w14:val="77000"/>
+																							<w14:alpha w14:val="', w_alpha, '"/>
 																						</w14:srgbClr>
 																					</w14:solidFill>
 																				</w14:textFill>
 																			</w:rPr>
-																			<w:t xml:space="preserve">   ', watermark, '</w:t>
+																			<w:t xml:space="preserve">', watermark, '</w:t>
 																		</w:r>
 																	</w:p>
 																</w:txbxContent>
@@ -225,83 +230,31 @@ insert_fake_watermark_XML <- function(doc, watermark, orientation) {
 											</wp:anchor>
 										</w:drawing>
 									</mc:Choice>
-									<mc:Fallback>
-										<w:pict>
-											<v:shapetype w14:anchorId="3648ABBD" id="_x0000_t202" coordsize="21600,21600" o:spt="202" path="m,l,21600r21600,l21600,xe">
-												<v:stroke joinstyle="miter"/>
-												<v:path gradientshapeok="t" o:connecttype="rect"/>
-											</v:shapetype>
-											<v:shape id="Text Box 2" o:spid="_x0000_s1026" type="#_x0000_t202" style="position:absolute;left:0;text-align:left;margin-left:', v3, 'pt;margin-top:', v4, 'pt;width:672.55pt;height:100.65pt;rotation:-2460854fd;z-index:251658240;visibility:visible;mso-wrap-style:square;mso-width-percent:0;mso-height-percent:0;mso-wrap-distance-left:9pt;mso-wrap-distance-top:0;mso-wrap-distance-right:9pt;mso-wrap-distance-bottom:0;mso-position-horizontal:absolute;mso-position-horizontal-relative:margin;mso-position-vertical:absolute;mso-position-vertical-relative:margin;mso-width-percent:0;mso-height-percent:0;mso-width-relative:page;mso-height-relative:page;v-text-anchor:top" o:gfxdata="UEsDBBQABgAIAAAAIQC2gziS/gAAAOEBAAATAAAAW0NvbnRlbnRfVHlwZXNdLnhtbJSRQU7DMBBF&#xA;90jcwfIWJU67QAgl6YK0S0CoHGBkTxKLZGx5TGhvj5O2G0SRWNoz/78nu9wcxkFMGNg6quQqL6RA&#xA;0s5Y6ir5vt9lD1JwBDIwOMJKHpHlpr69KfdHjyxSmriSfYz+USnWPY7AufNIadK6MEJMx9ApD/oD&#xA;OlTrorhX2lFEilmcO2RdNtjC5xDF9pCuTyYBB5bi6bQ4syoJ3g9WQ0ymaiLzg5KdCXlKLjvcW893&#xA;SUOqXwnz5DrgnHtJTxOsQfEKIT7DmDSUCaxw7Rqn8787ZsmRM9e2VmPeBN4uqYvTtW7jvijg9N/y&#xA;JsXecLq0q+WD6m8AAAD//wMAUEsDBBQABgAIAAAAIQA4/SH/1gAAAJQBAAALAAAAX3JlbHMvLnJl&#xA;bHOkkMFqwzAMhu+DvYPRfXGawxijTi+j0GvpHsDYimMaW0Yy2fr2M4PBMnrbUb/Q94l/f/hMi1qR&#xA;JVI2sOt6UJgd+ZiDgffL8ekFlFSbvV0oo4EbChzGx4f9GRdb25HMsYhqlCwG5lrLq9biZkxWOiqY&#xA;22YiTra2kYMu1l1tQD30/bPm3wwYN0x18gb45AdQl1tp5j/sFB2T0FQ7R0nTNEV3j6o9feQzro1i&#xA;OWA14Fm+Q8a1a8+Bvu/d/dMb2JY5uiPbhG/ktn4cqGU/er3pcvwCAAD//wMAUEsDBBQABgAIAAAA&#xA;IQCpdy+x9QEAAMUDAAAOAAAAZHJzL2Uyb0RvYy54bWysU01z0zAQvTPDf9DoTmyHhKaeOJ3QApcC&#xA;nWk6PSuyFBssrVgpsfPvWSluwtAbgw8aeyW9j93n5c1gOnZQ6FuwFS8mOWfKSqhbu6v40+bzuwVn&#xA;Pghbiw6sqvhReX6zevtm2btSTaGBrlbICMT6sncVb0JwZZZ52Sgj/AScsrSpAY0I9Im7rEbRE7rp&#xA;smmef8h6wNohSOU9Ve9Om3yV8LVWMnzX2qvAuoqTtpBWTOs2rtlqKcodCte0cpQh/kGFEa0l0jPU&#xA;nQiC7bF9BWVaieBBh4kEk4HWrVTJA7kp8r/cPDbCqeSFmuPduU3+/8HKb4dH94AsDB9hoAEmE97d&#xA;g/zpmYXbRtidWiNC3yhRE3HBz+Ukb3N0NNZU3aghfKpb6nER+5r1zpcjfpyHL31k2vZfoaYrYh8g&#xA;sQ0aDUOI167fz67y6UkE9YaRIhra8TwoImCSiov5rJgviFTSXjG9Wkxn14lSlBEtDsKhD18UGBZf&#xA;Ko6UhMQmDvc+RHWXI6PUqO6kMwzbgY5EyVuojyS6p4RU3P/aC1TUgL25BQoUCdAI5pkiuMZk+4V5&#xA;MzwLdCN3INkP3UtCkoAUlZpZYWIn6h8EZDoK3kF0bJ7TM7oZD49iT6jxroU1tU+3yclF5+iEspIM&#xA;jrmOYfzzO526/H2r3wAAAP//AwBQSwMEFAAGAAgAAAAhAKfNoAThAAAADAEAAA8AAABkcnMvZG93&#xA;bnJldi54bWxMj0FuwjAQRfeVegdrKnUHNkEQmsZBVVWkVu0mgQOYeEgi4nFkOxBuX7Nql6P/9P+b&#xA;fDuZnl3Q+c6ShMVcAEOqre6okXDY72YbYD4o0qq3hBJu6GFbPD7kKtP2SiVeqtCwWEI+UxLaEIaM&#xA;c1+3aJSf2wEpZifrjArxdA3XTl1juel5IsSaG9VRXGjVgO8t1udqNBJ8+fUxTmWV7KqD+9l/fg/m&#xA;hispn5+mt1dgAafwB8NdP6pDEZ2OdiTtWS9hli6TiEpYinUK7E4k6eIF2FHCSmwE8CLn/58ofgEA&#xA;AP//AwBQSwECLQAUAAYACAAAACEAtoM4kv4AAADhAQAAEwAAAAAAAAAAAAAAAAAAAAAAW0NvbnRl&#xA;bnRfVHlwZXNdLnhtbFBLAQItABQABgAIAAAAIQA4/SH/1gAAAJQBAAALAAAAAAAAAAAAAAAAAC8B&#xA;AABfcmVscy8ucmVsc1BLAQItABQABgAIAAAAIQCpdy+x9QEAAMUDAAAOAAAAAAAAAAAAAAAAAC4C&#xA;AABkcnMvZTJvRG9jLnhtbFBLAQItABQABgAIAAAAIQCnzaAE4QAAAAwBAAAPAAAAAAAAAAAAAAAA&#xA;AE8EAABkcnMvZG93bnJldi54bWxQSwUGAAAAAAQABADzAAAAXQUAAAAA&#xA;" o:allowincell="f" filled="f" stroked="f">
-												<v:stroke joinstyle="round"/>
-												<o:lock v:ext="edit" shapetype="t"/>
-												<v:textbox>
-													<w:txbxContent>
-														<w:p w14:paraId="72CD1A34" w14:textId="77777777" w:rsidR="00000000" w:rsidRDefault="00000000" w:rsidP="00C86F87">
-															<w:pPr>
-																<w:jc w:val="center"/>
-																<w:rPr>
-																	<w:rFonts w:ascii="Calibri" w:hAnsi="Calibri" w:cs="Calibri"/>
-																	<w:color w:val="000000"/>
-																	<w:sz w:val="72"/>
-																	<w:szCs w:val="72"/>
-																	<w14:textFill>
-																		<w14:solidFill>
-																			<w14:srgbClr w14:val="000000">
-																				<w14:alpha w14:val="77000"/>
-																			</w14:srgbClr>
-																		</w14:solidFill>
-																	</w14:textFill>
-																	<w14:ligatures w14:val="none"/>
-																</w:rPr>
-															</w:pPr>
-															<w:r>
-																<w:rPr>
-																	<w:rFonts w:ascii="Calibri" w:hAnsi="Calibri" w:cs="Calibri"/>
-																	<w:color w:val="000000"/>
-																	<w:sz w:val="72"/>
-																	<w:szCs w:val="72"/>
-																	<w14:textFill>
-																		<w14:solidFill>
-																			<w14:srgbClr w14:val="000000">
-																				<w14:alpha w14:val="77000"/>
-																			</w14:srgbClr>
-																		</w14:solidFill>
-																	</w14:textFill>
-																</w:rPr>
-																<w:t xml:space="preserve">   ', watermark, '</w:t>
-															</w:r>
-														</w:p>
-													</w:txbxContent>
-												</v:textbox>
-												<w10:wrap anchorx="margin" anchory="margin"/>
-											</v:shape>
-										</w:pict>
-									</mc:Fallback>
 								</mc:AlternateContent>
 							</w:r>
   ')
-  node_to_insert <- paste0(
-    '<w:tmp ',
-    '  xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main"',
-    '  xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing"',
-    '  xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml"',
-    '  xmlns:o="urn:schemas-microsoft-com:office:office"',
-    '  xmlns:v="urn:schemas-microsoft-com:vml"',
-    '  xmlns:w10="urn:schemas-microsoft-com:office:word"',
-    '  xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"',
-    '  xmlns:wp14="http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing"',
-    '  xmlns:wps="http://schemas.microsoft.com/office/word/2010/wordprocessingShape" mc:Ignorable="w14 w15 w16se w16cid w16 w16cex w16sdtdh w16sdtfl w16du wp14">',
-    '  xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main"',
-    '  xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"',
-    '>',
-    node_to_insert,
-    '</w:tmp>'
-  )
-  # nolint end
-  node_to_insert <- xml2::read_xml(node_to_insert)
-  node_to_insert <- xml2::xml_root(node_to_insert)
-  xml2::xml_add_sibling(node, node_to_insert, .where = "before")
-
+    node_to_insert <- paste0(
+      '<w:tmp ',
+      '  xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main"',
+      '  xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing"',
+      '  xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml"',
+      '  xmlns:o="urn:schemas-microsoft-com:office:office"',
+      '  xmlns:v="urn:schemas-microsoft-com:vml"',
+      '  xmlns:w10="urn:schemas-microsoft-com:office:word"',
+      '  xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"',
+      '  xmlns:wp14="http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing"',
+      '  xmlns:wps="http://schemas.microsoft.com/office/word/2010/wordprocessingShape" mc:Ignorable="w14 w15 w16se w16cid w16 w16cex w16sdtdh w16sdtfl w16du wp14">',
+      '  xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main"',
+      '  xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"',
+      '>',
+      node_to_insert,
+      '</w:tmp>'
+    )
+    # nolint end
+    node_to_insert <- xml2::read_xml(node_to_insert)
+    node_to_insert <- xml2::xml_root(node_to_insert)
+    xml2::xml_add_sibling(node, node_to_insert, .where = "before")
+  }
 }
 
 insert_watermark_XML <- function(doc, watermark) {
@@ -1903,7 +1856,7 @@ export_as_docx_j <- function(
   } else {
 
     if (is.null(template_file)) {
-      template_file <- get_template_file(watermark = watermark,
+      template_file <- get_template_file(watermark = NULL,
                                          orientation = orientation,
                                          pagenum = pagenum)
     }
@@ -1927,9 +1880,10 @@ export_as_docx_j <- function(
       section_properties$footer_default <- footer_default
     }
     # END
-    if (is.null(watermark)) {
-      doc <- officer::body_set_default_section(doc, section_properties)
-    }
+    # if (is.null(watermark)) {
+    #   doc <- officer::body_set_default_section(doc, section_properties)
+    # }
+    doc <- officer::body_set_default_section(doc, section_properties)
 
     flex_tbl_list <- lapply(flex_tbl_list, function(flx) {
       if (flx$properties$layout != "autofit") {
@@ -2015,8 +1969,9 @@ export_as_docx_j <- function(
     add_vertical_pagination_XML(doc)
     remove_security_popup_page_numbers_XML(doc, tlgtype, pagenum)
     if (!is.null(watermark)) {
-      remove_table_shading_XML(doc)
-      insert_watermark_XML(doc, watermark)
+      # remove_table_shading_XML(doc)
+      # insert_watermark_XML(doc, watermark)
+      insert_fake_watermark_XML(doc, watermark, orientation)
     }
 
     print(doc, target = paste0(output_dir, "/", tolower(tblid), ".docx"))
