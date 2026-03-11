28168af5207f1b411566d2fd69e923611d32031b
munoztd0 <david.munoztord@mailbox.org>
2026-03-11 17:00:59 +0100

    update: add test for geom_j.R

--- tests/testthat/test-geom_j.R
+++ tests/testthat/test-geom_j.R
@@ -1,5 +1,8 @@
-test_that("StatBoxplotSas computes type = 2 quantiles and whiskers", {
-  skip_if_not_installed("ggplot2")
+suppressPackageStartupMessages({
+  library(ggplot2)
+})
+
+test_that("StatBoxplotSas computes type = 2 quantiles", {
 
   vec <- c(1, 2, 3, 4, 100) # force outlier
   df <- data.frame(x = 1, y = vec)
@@ -16,16 +19,15 @@ test_that("StatBoxplotSas computes type = 2 quantiles and whiskers", {
   ymax_exp <- max(vec[vec <= upper_fence])
   out_exp <- vec[vec < ymin_exp | vec > ymax_exp]
 
-  expect_equal(res$lower, unname(qs[2]))
-  expect_equal(res$middle, unname(qs[3]))
-  expect_equal(res$upper, unname(qs[4]))
+  expect_equal(res$lower, qs[[2]])
+  expect_equal(res$middle, qs[[3]])
+  expect_equal(res$upper, qs[[4]])
   expect_equal(res$ymin, ymin_exp)
   expect_equal(res$ymax, ymax_exp)
   expect_setequal(unlist(res$outliers), out_exp)
 })
 
 test_that("geom_boxplot_j uses type = 2 quantiles", {
-  skip_if_not_installed("ggplot2")
 
   set.seed(123)
   df <- data.frame(
@@ -43,16 +45,16 @@ test_that("geom_boxplot_j uses type = 2 quantiles", {
   split_quantile_2 <- lapply(split(df$y, df$g), stats::quantile,
                      probs = c(0.25, 0.5, 0.75), type = 2)
 
-  exp_lower <- vapply(split_quantile_2, function(q) unname(q[1]), numeric(1))
-  exp_middle <- vapply(split_quantile_2, function(q) unname(q[2]), numeric(1))
-  exp_upper <- vapply(split_quantile_2, function(q) unname(q[3]), numeric(1))
+  exp_lower <- vapply(split_quantile_2, function(q) q[[1]], numeric(1))
+  exp_middle <- vapply(split_quantile_2, function(q) q[[2]], numeric(1))
+  exp_upper <- vapply(split_quantile_2, function(q) q[[3]], numeric(1))
 
   expect_equal(d$lower, as.numeric(exp_lower))
   expect_equal(d$middle, as.numeric(exp_middle))
   expect_equal(d$upper, as.numeric(exp_upper))
 })
 
-test_that("StatBoxplotSas quantiles differ from default type = 7 on a known dataset", {
+test_that("StatBoxplotSas differs from default type = 7", {
   df <- data.frame(x = 1, y = 1:8)
 
   res <- junco:::StatBoxplotSas$compute_group(
@@ -63,14 +65,13 @@ test_that("StatBoxplotSas quantiles differ from default type = 7 on a known data
   q7 <- stats::quantile(1:8, probs = c(0.25, 0.5, 0.75), type = 7) #default R
 
   # sanity check matches type=2
-  expect_equal(c(res$lower, res$middle, res$upper), as.numeric(unname(q2)))
+  expect_equal(c(res$lower, res$middle, res$upper), as.numeric(q2))
 
   #doesn't match type=7
-  expect_true(any(as.numeric(unname(q2)) != as.numeric(unname(q7))))
+  expect_true(any(as.numeric(q2) != as.numeric(q7)))
 })
 
-test_that("geom_boxplot_j output differs from default type = 7 quantiles on a known dataset", {
-  skip_if_not_installed("ggplot2")
+test_that("geom_boxplot_j differs from default type = 7", {
 
   df <- data.frame(g = factor("a"), y = 1:8)
 
@@ -82,8 +83,8 @@ test_that("geom_boxplot_j output differs from default type = 7 quantiles on a kn
   q7 <- stats::quantile(df$y, probs = c(0.25, 0.5, 0.75), type = 7) #default R
 
   # sanity check matches type=2
-  expect_equal(c(d$lower, d$middle, d$upper), as.numeric(unname(q2)))
+  expect_equal(c(d$lower, d$middle, d$upper), as.numeric(q2))
 
   #doesn't match type=7
-  expect_true(any(as.numeric(unname(q2)) != as.numeric(unname(q7))))
+  expect_true(any(as.numeric(q2) != as.numeric(q7)))
 })
