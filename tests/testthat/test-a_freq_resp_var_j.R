13095ab2c7048f278974b13df9bbebafbc5c566d
David Muñoz Tord <david.munoz@mailbox.org>
2026-05-04 11:24:29 +0200

    Lintr issues dev (#305)
    * update: lifecycle
    
    * update: lintr
    
    * update: last lintr

--- tests/testthat/test-a_freq_resp_var_j.R
+++ tests/testthat/test-a_freq_resp_var_j.R
@@ -230,7 +230,7 @@ test_that("a_freq_resp_var_j in layout with relative risk column for combined fa
   library(dplyr)
   trtvar <- "ARM"
   ctrl_grp <- "B: Placebo"
-  
+
   adsl <- ex_adsl |> select(c("USUBJID", "SEX", all_of(trtvar)))
   adsl$colspan_trt <- factor(
     ifelse(adsl[[trtvar]] == ctrl_grp, " ", "Active Study Agent"),
@@ -238,16 +238,15 @@ test_that("a_freq_resp_var_j in layout with relative risk column for combined fa
   )
   adsl$rrisk_header <- "Risk Difference (%) (95% CI)"
   adsl$rrisk_label <- paste(adsl[[trtvar]], paste("vs", ctrl_grp))
-  
-  had_ae <- ex_adae %>% 
-    mutate(TRTEMFL = "Y") |> 
-    select(USUBJID, TRTEMFL) %>%
+  had_ae <- ex_adae |>
+    mutate(TRTEMFL = "Y") |>
+    select(USUBJID, TRTEMFL) |>
     distinct(USUBJID, .keep_all = TRUE)
-  
-  adsl <- adsl %>%
-    left_join(had_ae) %>%
-    mutate(TRTEMFL = ifelse(is.na(TRTEMFL), "N", "Y"))  
-  
+
+  adsl <- adsl |>
+    left_join(had_ae) |>
+    mutate(TRTEMFL = ifelse(is.na(TRTEMFL), "N", "Y"))
+
   colspan_trt_map <- create_colspan_map(
     df = adsl,
     non_active_grp = ctrl_grp,
@@ -256,20 +255,21 @@ test_that("a_freq_resp_var_j in layout with relative risk column for combined fa
     colspan_var = "colspan_trt",
     trt_var = trtvar
   )
-  
+
   a_freq_j_args <- list(
     resp_var = "TRTEMFL", drop_levels = TRUE,
     riskdiff = TRUE,
     ref_path = c("colspan_trt", " ", trtvar, ctrl_grp),
     method = "wald"
   )
-  
+
   # Set up levels and label for the required combined columns
-  add_combo <- add_combo_facet("Combined",
-                               label = "Combined",
-                               levels = setdiff(unique(adsl[[trtvar]]), ctrl_grp)
+  add_combo <- add_combo_facet(
+    "Combined",
+    label = "Combined",
+    levels = setdiff(unique(adsl[[trtvar]]), ctrl_grp)
   )
-  
+
   # choose if any facets need to be removed - e.g remove the combined column for placebo
   rm_combo_from_placebo <- cond_rm_facets(
     facets = "Combined",
@@ -277,9 +277,9 @@ test_that("a_freq_resp_var_j in layout with relative risk column for combined fa
     value = " ",
     split = "colspan_trt"
   )
-  
-  mysplit_comb <- make_split_fun(post = list(add_combo, rm_combo_from_placebo))  
-  
+
+  mysplit_comb <- make_split_fun(post = list(add_combo, rm_combo_from_placebo))
+
   # choose if any facets need to be removed - e.g remove the combined column for placebo
   rm_combo_from_placebo2 <- cond_rm_facets(
     facets = ctrl_grp,
@@ -287,47 +287,46 @@ test_that("a_freq_resp_var_j in layout with relative risk column for combined fa
     value = "Risk Difference (%) (95% CI)",
     split = "rrisk_header"
   )
-  add_combo2 <- add_combo_facet("Combined",
-                                label = paste0("Combined vs ", ctrl_grp),
-                                levels = setdiff(unique(adsl[[trtvar]]), ctrl_grp)
+  add_combo2 <- add_combo_facet(
+    "Combined",
+    label = paste0("Combined vs ", ctrl_grp),
+    levels = setdiff(unique(adsl[[trtvar]]), ctrl_grp)
   )
-  
+
   mysplit_comb2 <- make_split_fun(post = list(add_combo2, rm_combo_from_placebo2))
-  
-  
   lyt <- basic_table(show_colcounts = TRUE) |>
     split_cols_by("colspan_trt", split_fun = trim_levels_to_map(map = colspan_trt_map)) |>
-    split_cols_by(trtvar, split_fun = mysplit_comb) |> 
+    split_cols_by(trtvar, split_fun = mysplit_comb) |>
     split_cols_by("rrisk_header", nested = FALSE) |>
     split_cols_by(trtvar, labels_var = "rrisk_label", split_fun = mysplit_comb2) |>
-    analyze("SEX", 
-            show_labels = "visible",
-            afun = a_freq_resp_var_j,            
-            extra_args = a_freq_j_args)
-  
-  
+    analyze(
+      "SEX",
+      show_labels = "visible",
+      afun = a_freq_resp_var_j,
+      extra_args = a_freq_j_args
+    )
   result <- build_table(lyt, adsl, alt_counts_df = adsl)
-  
+
   # confirm column is not NE (NE, NE) and numbers are correct
   actual_row <- result[2, ]
   actual <- cell_values(result[2, 7])
-  
-  
+
   combcols <- 1:2
   ctrlcol <- 4
 
-  
   countscomb <- sum(sapply(cell_values(actual_row[1, combcols]), function(v) v[["count_unique"]]), na.rm = TRUE)
   countsctrl <- cell_values(actual_row[1, ctrlcol])[[1]][["count_unique"]]
-  
+
   denomcomb <- sum(sapply(cell_values(actual_row[1, combcols]), function(v) v[["d"]]), na.rm = TRUE)
   denomctrl <- cell_values(actual_row[1, ctrlcol])[[1]][["d"]]
-  
+
   # default method for relative risk in a_freq_j is wald stat which is based upon tern function
-  expected <- tern::stat_propdiff_ci( x = list(countscomb), 
-                                      y = list(countsctrl), 
-                                      N_x = denomcomb, 
-                                      N_y = denomctrl)
-  
+  expected <- tern::stat_propdiff_ci(
+    x = list(countscomb),
+    y = list(countsctrl),
+    N_x = denomcomb,
+    N_y = denomctrl
+  )
+
   testthat::expect_equal(actual, expected, ignore_attr = TRUE)
 })
