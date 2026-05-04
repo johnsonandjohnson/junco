b1ca12e5943201f2f72e077e4d24466cf34be5a3
munoztd0 <david.munoztord@mailbox.org>
2026-05-04 12:06:33 +0200

    update: dontrun{}

--- R/safe_t_test.r
+++ R/safe_t_test.r
@@ -32,7 +32,7 @@
 #' safe_t_test(1:10, 11:20)
 #'
 #' # Example triggering failure (zero variance)
-#' try(stats::t.test(rep(10, 5), rep(10, 5)), silent = TRUE)
+#' dontrun{stats::t.test(rep(10, 5), rep(10, 5))}
 #' safe_t_test(rep(10, 5), rep(10, 5))
 safe_t_test <- function(x, y = NULL, ...) {
   x_expr <- substitute(x)
