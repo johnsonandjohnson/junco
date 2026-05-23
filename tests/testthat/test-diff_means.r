a0e22d88351d15f962bf1006a44802ff9247a690
Wojtek <wojciech.wojciak@gmail.com>
2026-05-23 13:47:46 +0200

    updated a_diff_means(), added unit tests for a_diff_means().

--- tests/testthat/test-diff_means.r
+++ tests/testthat/test-diff_means.r
@@ -1,10 +1,18 @@
+suppressPackageStartupMessages({
+  library(dplyr)
+})
+
+# s_diff_means ----
+
+## data setup ----
+
 diff_means_stat_names <- paste0(
   "diff_means_", c("n1", "n2", "est", "se", "est_se", "ci", "est_ci")
 )
 
-# Start of tests ----
+## tests ----
 
-testthat::test_that("s_diff_means works for unpaired data", {
+test_that("s_diff_means works for unpaired data", {
   df1 <- data.frame(x = c(1, 2, 2, 3))
   df2 <- data.frame(x = c(4, 5, 4, 8, 10))
 
@@ -24,7 +32,7 @@ testthat::test_that("s_diff_means works for unpaired data", {
   expect_identical(res$diff_means_est_ci, c(est, ci), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means works for unpaired data (NA, NaN)", {
+test_that("s_diff_means works for unpaired data (NA, NaN)", {
   df1 <- data.frame(x = c(1, 2, NA, 3))
   df2 <- data.frame(x = c(4, 5, NaN, 8, 10))
 
@@ -44,7 +52,7 @@ testthat::test_that("s_diff_means works for unpaired data (NA, NaN)", {
   expect_identical(res$diff_means_est_ci, c(est, ci), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means works for paired data", {
+test_that("s_diff_means works for paired data", {
   df1 <- data.frame(id = c("A", "B", "C", "D", "E"), x = c(1, 2, 3, 21, 5))
   df2 <- data.frame(id = c("D", "G", "B", "A"), x = c(12, 31, 3, 5))
 
@@ -66,7 +74,7 @@ testthat::test_that("s_diff_means works for paired data", {
   expect_identical(res$diff_means_est_ci, c(est, ci), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means works for paired data (NA, NaN)", {
+test_that("s_diff_means works for paired data (NA, NaN)", {
   df1 <- data.frame(id = c("A", "B", "C", "D", "E"), x = c(1, 2, 3, NA, 5))
   df2 <- data.frame(id = c("D", "E", "C", "B", "A", "W"), x = c(9, 14, 31, 3, NaN, 8))
 
@@ -88,7 +96,7 @@ testthat::test_that("s_diff_means works for paired data (NA, NaN)", {
   expect_identical(res$diff_means_est_ci, c(est, ci), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means handles identical paired vectors", {
+test_that("s_diff_means handles identical paired vectors", {
   df1 <- data.frame(id = 1:3, x = c(1, 8, 23))
   df2 <- df1
 
@@ -104,7 +112,7 @@ testthat::test_that("s_diff_means handles identical paired vectors", {
   expect_identical(res$diff_means_est_ci, c(0, NaN, NaN), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means handles single observation groups", {
+test_that("s_diff_means handles single observation groups", {
   df1 <- data.frame(x = 10, NA, NA)
   df2 <- data.frame(x = 5, NA)
 
@@ -120,7 +128,7 @@ testthat::test_that("s_diff_means handles single observation groups", {
   expect_identical(res$diff_means_est_ci, c(5, c(NA_real_, NA_real_)), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means handles single observation groups (paired)", {
+test_that("s_diff_means handles single observation groups (paired)", {
   df1 <- data.frame(id = c("A", "B", "C"), x = c(10, NA, NA))
   df2 <- data.frame(id = c("C", "A"), x = c(NA, 5))
 
@@ -136,7 +144,7 @@ testthat::test_that("s_diff_means handles single observation groups (paired)", {
   expect_identical(res$diff_means_est_ci, c(5, c(NA_real_, NA_real_)), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means handles empty data", {
+test_that("s_diff_means handles empty data", {
   df1 <- data.frame(x = numeric())
   df2 <- df1
 
@@ -152,7 +160,7 @@ testthat::test_that("s_diff_means handles empty data", {
   expect_identical(res$diff_means_est_ci, c(NA_real_, c(NA_real_, NA_real_)), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means handles empty data (paired)", {
+test_that("s_diff_means handles empty data (paired)", {
   df1 <- data.frame(id = factor(), x = numeric())
   df2 <- df1
 
@@ -168,7 +176,7 @@ testthat::test_that("s_diff_means handles empty data (paired)", {
   expect_identical(res$diff_means_est_ci, c(NA_real_, c(NA_real_, NA_real_)), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means handles empty df2", {
+test_that("s_diff_means handles empty df2", {
   df1 <- data.frame(x = c(2, 2, 2))
   df2 <- data.frame(x = numeric())
 
@@ -184,7 +192,7 @@ testthat::test_that("s_diff_means handles empty df2", {
   expect_identical(res$diff_means_est_ci, c(NA_real_, c(NA_real_, NA_real_)), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means handles empty df2 (paired)", {
+test_that("s_diff_means handles empty df2 (paired)", {
   df1 <- data.frame(id = c("A", "B", "C"), x = c(2, 2, 2))
   df2 <- data.frame(id = factor(), x = numeric())
 
@@ -200,7 +208,7 @@ testthat::test_that("s_diff_means handles empty df2 (paired)", {
   expect_identical(res$diff_means_est_ci, c(NA_real_, c(NA_real_, NA_real_)), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means handles empty df1", {
+test_that("s_diff_means handles empty df1", {
   df1 <- data.frame(x = numeric())
   df2 <- data.frame(x = c(2, 2, 2))
 
@@ -216,7 +224,7 @@ testthat::test_that("s_diff_means handles empty df1", {
   expect_identical(res$diff_means_est_ci, c(NA_real_, c(NA_real_, NA_real_)), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means handles empty df1 (paired)", {
+test_that("s_diff_means handles empty df1 (paired)", {
   df1 <- data.frame(id = factor(), x = numeric())
   df2 <- data.frame(id = c("A", "B", "C"), x = c(2, 2, 2))
 
@@ -232,7 +240,7 @@ testthat::test_that("s_diff_means handles empty df1 (paired)", {
   expect_identical(res$diff_means_est_ci, c(NA_real_, c(NA_real_, NA_real_)), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means returns NA when no matching IDs", {
+test_that("s_diff_means returns NA when no matching IDs", {
   df1 <- data.frame(id = c("A", "B"), x = 1:2)
   df2 <- data.frame(id = c("C", "D"), x = c(4, 6))
 
@@ -248,7 +256,7 @@ testthat::test_that("s_diff_means returns NA when no matching IDs", {
   expect_identical(res$diff_means_est_ci, c(NA_real_, c(NA_real_, NA_real_)), ignore_attr = TRUE)
 })
 
-testthat::test_that("s_diff_means handles zero variance", {
+test_that("s_diff_means handles zero variance", {
   df1 <- data.frame(x = c(2, 2, 2))
   df2 <- data.frame(x = c(1, 1, 1, NA))
 
@@ -265,7 +273,7 @@ testthat::test_that("s_diff_means handles zero variance", {
 })
 
 
-testthat::test_that("s_diff_means errors on duplicate paired IDs", {
+test_that("s_diff_means errors on duplicate paired IDs", {
   df1 <- data.frame(id = c("A", "A"), x = 1:2)
   df2 <- data.frame(id = c("A", "B"), x = 3:4)
 
@@ -275,44 +283,281 @@ testthat::test_that("s_diff_means errors on duplicate paired IDs", {
   )
 })
 
-testthat::test_that("s_diff_means errors if paired_by missing in paired mode", {
+test_that("s_diff_means errors if paired_by missing in paired mode", {
   df1 <- data.frame(id = "A", x = 1)
   df2 <- data.frame(id = "A", x = 2)
 
   expect_error(s_diff_means(df1, df2, "x", paired = TRUE), "paired_by")
 })
 
-testthat::test_that("s_diff_means errors for missing variable", {
+test_that("s_diff_means errors for missing variable", {
   df1 <- data.frame(x = 1:3)
   df2 <- data.frame(x = 4:6)
 
   expect_error(s_diff_means(df1, df2, "y"), "df1.*y")
 })
 
-testthat::test_that("s_diff_means errors for non-numeric input (char)", {
+test_that("s_diff_means errors for non-numeric input (char)", {
   df1 <- data.frame(x = letters[1:3])
   df2 <- data.frame(x = letters[4:6])
 
   expect_error(s_diff_means(df1, df2, "x"), "\\.var.*numeric")
 })
 
-testthat::test_that("s_diff_means errors for Inf values", {
+test_that("s_diff_means errors for Inf values", {
   df1 <- data.frame(x = c(1, Inf))
   df2 <- data.frame(x = 2)
 
   expect_error(s_diff_means(df1, df2, "x"), "finite")
 })
 
-testthat::test_that("s_diff_means errors for non-numeric input (char)", {
+test_that("s_diff_means errors for non-numeric input (char)", {
   df1 <- data.frame(x = letters[1:3])
   df2 <- data.frame(x = letters[4:6])
 
   expect_error(s_diff_means(df1, df2, "x"), "\\.var.*numeric")
 })
 
-testthat::test_that("s_diff_means errors for non-numeric input (factor)", {
+test_that("s_diff_means errors for non-numeric input (factor)", {
   df1 <- data.frame(x = factor(letters[1:3]))
   df2 <- data.frame(x = factor(letters[4:6]))
 
   expect_error(s_diff_means(df1, df2, "x"), "\\.var.*numeric")
+})
+
+# a_diff_means ----
+
+## data setup ----
+
+advs_bp_blw12 <- local({
+  ex_advs |>
+    filter(
+      PARAMCD %in% c("DIABP", "SYSBP"),
+      AVISIT %in% c("BASELINE", "WEEK 1 DAY 8", "WEEK 2 DAY 15")
+    ) |>
+    mutate(
+      PARAMCD = droplevels(PARAMCD),
+      AVISIT = droplevels(AVISIT)
+    )
+})
+
+advs2 <- local({
+  set.seed(123)
+  df <- expand.grid(
+    ID = c("01", "02", "03", "04"),
+    ARM = c("Placebo", "Drug X", "Drug Y"),
+    AVISIT = c("Baseline", "Week 12", "Week 24")
+  )
+  # remove some Week 24 records (irregular missing structure)
+  drop_w24 <- which(df$AVISIT == "Week 24")[c(2, 5, 9)]
+  df <- df[-drop_w24, ]
+  df$AVAL <- sample(110:140, nrow(df), replace = TRUE)
+  df$AVAL[c(4, 11)] <- NA
+  df$ARM <- ordered(df$ARM, levels = c("Drug X", "Drug Y", "Placebo"))
+  df
+})
+
+## tests ----
+
+test_that("a_diff_means works in a table layout with ex_advs data", {
+  lyt <- basic_table() |>
+    split_cols_by("ARM") |>
+    split_rows_by("PARAMCD") |>
+    split_rows_by("AVISIT") |>
+    analyze("AVAL",
+      afun = a_diff_means,
+      extra_args = list(ref_path = c("ARM", "B: Placebo"))
+    )
+
+  res <- expect_silent(build_table(lyt, advs_bp_blw12))
+  res_matrix <- matrix_form(res)$string
+
+  expect_snapshot(res_matrix)
+})
+
+test_that("a_diff_means works in a table layout (stacked header) with ex_advs data", {
+  advs_bp_blw12$colspan_trt <- factor(
+    ifelse(advs_bp_blw12$ARM == "B: Placebo", " ", "Active Study Agent"),
+    levels = c("Active Study Agent", " ")
+  )
+  colsp_trt_map <- create_colspan_map(
+    advs_bp_blw12,
+    non_active_grp = "B: Placebo",
+    non_active_grp_span_lbl = " ",
+    active_grp_span_lbl = "Active Study Agent",
+    colspan_var = "colspan_trt",
+    trt_var = "ARM"
+  )
+
+  lyt <- basic_table() |>
+    split_cols_by("colspan_trt", split_fun = trim_levels_to_map(map = colsp_trt_map)) |>
+    split_cols_by("ARM") |>
+    split_rows_by("PARAMCD") |>
+    split_rows_by("AVISIT") |>
+    analyze("AVAL",
+      afun = a_diff_means,
+      extra_args = list(ref_path = c("colspan_trt", " ", "ARM", "B: Placebo"))
+    )
+
+  res <- expect_silent(build_table(lyt, advs_bp_blw12))
+  res_matrix <- matrix_form(res)$string
+
+  expect_snapshot(res_matrix)
+})
+
+test_that("a_diff_means works in a table layout with synthetic paired data containing NA values", {
+  lyt <- basic_table() |>
+    split_cols_by("ARM") |>
+    split_rows_by("AVISIT") |>
+    analyze("AVAL",
+      afun = a_diff_means,
+      extra_args = list(ref_path = c("ARM", "Placebo"), paired = TRUE, paired_by = "ID")
+    )
+
+  res <- expect_silent(build_table(lyt, advs2))
+  res_matrix <- matrix_form(res)$string
+
+  expect_snapshot(res_matrix)
+})
+
+test_that("a_diff_means works with custom .stats, .formats, .labels, .indents", {
+  df <- advs_bp_blw12[advs_bp_blw12$ARM == "A: Drug X", ]
+  ref_path <- c("ARM", "B: Placebo")
+  spl_context <- data.frame(
+    full_parent_df = I(list(advs_bp_blw12)),
+    cur_col_split = I(list("ARM")),
+    cur_col_split_val = I(list("A: Drug X")),
+    "B: Placebo" = I(list(advs_bp_blw12$ARM == "B: Placebo")),
+    check.names = FALSE
+  )
+  .formats <- c(diff_means_est_ci = jjcsformat_xx("xx.xx (xx.xx, xx.xx)"))
+  .labels <- c(diff_means_est_ci = "diff mean est + CI")
+
+  res <- expect_silent(
+    a_diff_means(
+      df = df,
+      .var = "AVAL",
+      .spl_context = spl_context,
+      ref_path = ref_path,
+      .stats = "diff_means_est_ci",
+      .formats = .formats,
+      .labels = .labels,
+      .indent_mods = 4L
+    )
+  )
+
+  # expected
+  exp_est <- list(
+    diff_means_est_ci = c(
+      diff_means_est = 0.076020,
+      diff_means_ci_lwr = -0.723132,
+      diff_means_ci_upr = 0.875172
+    )
+  )
+  attr(exp_est$diff_means_est_ci, "conf.level") <- 0.95
+  attr(exp_est$diff_means_est_ci, "label") <- "Difference in Means (95% CI)"
+
+  exp <- in_rows(
+    .list = exp_est,
+    .formats = .formats,
+    .names = "diff_means_est_ci",
+    .labels = .labels,
+    .indent_mods = 4L
+  )
+
+  expect_equal(res, exp, tolerance = 10^-6)
+})
+
+test_that("a_diff_means works when in ref column", {
+  df <- advs_bp_blw12[advs_bp_blw12$ARM == "A: Drug X", ]
+  ref_path <- c("ARM", "B: Placebo")
+  spl_context <- data.frame(
+    full_parent_df = I(list(advs_bp_blw12)),
+    cur_col_split = I(list("ARM")),
+    cur_col_split_val = I(list("B: Placebo")),
+    "B: Placebo" = I(list(advs_bp_blw12$ARM == "B: Placebo")),
+    check.names = FALSE
+  )
+
+  res <- expect_silent(
+    a_diff_means(
+      df = df,
+      .var = "AVAL",
+      .spl_context = spl_context,
+      ref_path = ref_path,
+    )
+  )
+
+  expect_snapshot(res)
+})
+
+test_that("a_diff_means works with no observations in the column-split sample", {
+  df <- advs_bp_blw12[FALSE, ]
+  ref_path <- c("ARM", "B: Placebo")
+  spl_context <- data.frame(
+    full_parent_df = I(list(advs_bp_blw12)),
+    cur_col_split = I(list("ARM")),
+    cur_col_split_val = I(list("A: Drug X")),
+    "B: Placebo" = I(list(advs_bp_blw12$ARM == "B: Placebo")),
+    check.names = FALSE
+  )
+
+  res <- expect_silent(
+    a_diff_means(
+      df = df,
+      .var = "AVAL",
+      .spl_context = spl_context,
+      ref_path = ref_path,
+    )
+  )
+
+  expect_snapshot(res)
+})
+
+test_that("a_diff_means works with no observations in the ref sample", {
+  df <- advs_bp_blw12[advs_bp_blw12$ARM == "A: Drug X", ]
+  full_parent_df <- advs_bp_blw12[advs_bp_blw12$ARM != "B: Placebo", ]
+  ref_path <- c("ARM", "B: Placebo")
+  spl_context <- data.frame(
+    full_parent_df = I(list(full_parent_df)),
+    cur_col_split = I(list("ARM")),
+    cur_col_split_val = I(list("A: Drug X")),
+    "B: Placebo" = I(list(full_parent_df$ARM == "B: Placebo")),
+    check.names = FALSE
+  )
+
+  res <- expect_silent(
+    a_diff_means(
+      df = df,
+      .var = "AVAL",
+      .spl_context = spl_context,
+      ref_path = ref_path,
+    )
+  )
+
+  expect_snapshot(res)
+})
+
+test_that("a_diff_means works with no observations in both samples", {
+  df <- advs_bp_blw12[FALSE, ]
+  full_parent_df <- advs_bp_blw12[advs_bp_blw12$ARM != "B: Placebo", ]
+  ref_path <- c("ARM", "B: Placebo")
+  spl_context <- data.frame(
+    full_parent_df = I(list(full_parent_df)),
+    cur_col_split = I(list("ARM")),
+    cur_col_split_val = I(list("A: Drug X")),
+    "B: Placebo" = I(list(full_parent_df$ARM == "B: Placebo")),
+    check.names = FALSE
+  )
+
+  res <- expect_silent(
+    a_diff_means(
+      df = df,
+      .var = "AVAL",
+      .spl_context = spl_context,
+      ref_path = ref_path,
+    )
+  )
+
+  expect_snapshot(res)
 })
