19e899c729fc6aa5fe6401c0bd4c87172fa20a3e
munoztd0 <david.munoztord@mailbox.org>
2026-06-01 10:54:00 +0200

    fix: add s_coxph_pairwise

--- R/coxph_hr.R
+++ R/coxph_hr.R
@@ -1,7 +1,7 @@
-#' @name  coxph_hr
-#' @title coxph_hr
-#' @description coxph_hr `r lifecycle::badge("deprecated")` Use
-#'   `tern::s_coxph_pairwise` and `tern:::a_coxph_pairwise()` instead, and use `tern::coxph_pairwise()` as
+#' @name  a_coxph_hr
+#' @title a_coxph_hr
+#' @description a_coxph_hr `r lifecycle::badge("deprecated")` Use
+#'   tern:::a_coxph_pairwise()` instead, and use `tern::coxph_pairwise()` as
 #'   the layout-level wrapper.
 #' @inheritParams odds_ratio
 #' @export
@@ -16,9 +16,35 @@ a_coxph_hr <- function(
     .labels = NULL,
     .indent_mods = NULL) {
   lifecycle::deprecate_stop(
-    when = "0.1.0",
+    when = "0.1.6",
     what = "a_coxph_hr()",
     with = "tern:::a_coxph_pairwise()",
     details = "Use `tern:::coxph_pairwise()` as the layout-level wrapper."
   )
 }
+
+#' @name  coxph_hr
+#' @title coxph_hr
+#' @description coxph_hr `r lifecycle::badge("deprecated")` Use
+#'  `tern::s_coxph_pairwise`  instead, and use `tern::coxph_pairwise()` as
+#'   the layout-level wrapper.
+#' @inheritParams odds_ratio
+#' @export
+a_coxph_hr <- function(
+    df,
+    .var,
+    ref_path,
+    .spl_context,
+    ...,
+    .stats = NULL,
+    .formats = NULL,
+    .labels = NULL,
+    .indent_mods = NULL) {
+  lifecycle::deprecate_stop(
+    when = "0.1.6",
+    what = "s_coxph_hr()",
+    with = "tern:::s_coxph_pairwise()",
+    details = "Use `tern:::coxph_pairwise()` as the layout-level wrapper."
+  )
+}
+
--- tests/testthat/test-coxph_hr.R
+++ tests/testthat/test-coxph_hr.R
@@ -5,3 +5,12 @@ test_that("a_coxph_hr is deprecated", {
     )
   )
 })
+
+
+test_that("s_coxph_hr is deprecated", {
+  expect_error(
+    lifecycle::expect_deprecated(
+      s_coxph_hr(df = data.frame(), .var = "x", ref_path = NULL, .spl_context = NULL)
+    )
+  )
+})
